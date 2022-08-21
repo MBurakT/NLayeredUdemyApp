9238cafd1bb1a83eab6a07ce86a6237b3754be63
fkymon <mbtbrk11@outlook.com>
2022-08-21 16:37:34 +0300

    Bölüm 4.20

---
+++ NLayer.Core/Category.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace NLayer.Core
+{
+    public class Category : BaseEntity
+    {
+        public string Name { get; set; }
+
+        //Navigation Property
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ NLayer.Core/Product.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace NLayer.Core
+{
+    public class Product : BaseEntity
+    {
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public decimal Price { get; set; }
+
+        // Foreign Key
+
+        public int CategoryId { get; set; }
+
+        //Navigation Property
+
+        public Category Category { get; set; }
+
+        public ProductFeature ProductFeature { get; set; }
+    }
+}
