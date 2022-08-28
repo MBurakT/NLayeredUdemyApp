23fdc644bb62f2805eab15095e9a4a6a758cb7e6
fkymon <mbtbrk11@outlook.com>
2022-08-28 17:44:00 +0300

    Bölüm 5.26

--- NLayer.Repository/Repositories/GenericRepository.cs
+++ NLayer.Repository/Repositories/GenericRepository.cs
@@ -6,7 +6,6 @@ namespace NLayer.Repository.Repositories
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
-
         protected readonly AppDbContext _context;
         private readonly DbSet<T> _dbSet;
 
