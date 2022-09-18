aa0f75e66700d0a8cec35457fd8c4f98d2ce3e84
fkymon <mbtbrk11@outlook.com>
2022-09-18 18:19:24 +0300

    Bölüm 8.49

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -1,20 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NLayer.Core.Dtos;
+using NLayer.Core.Models;
 using NLayer.Core.Services;
 
 namespace NLayer.Web.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly IMapper _mapper;
         private readonly IProductService _service;
+        private readonly ICategoryService _categoryService; 
 
-        public ProductsController(IProductService service)
+        public ProductsController(IMapper mapper, IProductService service, ICategoryService categoryService)
         {
+            _mapper = mapper;
             _service = service;
+            _categoryService = categoryService;
         }
 
         public async Task<IActionResult> Index()
         {
             return View(await _service.GetProductsWithCategoryAsync());
+        }
+
+        public async Task<IActionResult> Save()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+
+            return View();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                await _service.AddAsync(_mapper.Map<Product>(productDto));
+                return RedirectToAction(nameof(Index));
+            }
+
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
+
+            return View();
         }
     }
 }
