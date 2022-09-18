d20f970e29ec54614c85cb3af348d7a0a225846b
fkymon <mbtbrk11@outlook.com>
2022-09-18 15:10:26 +0300

    Bölüm 8.47

--- NLayer.Service/Sercives/ProductService.cs
+++ NLayer.Service/Sercives/ProductService.cs
@@ -9,12 +9,12 @@ using System.Collections.Generic;
 
 namespace NLayer.Service.Sercives
 {
-    public class ProductServiceWithNoCaching : Service<Product>, IProductService
+    public class ProductService : Service<Product>, IProductService
     {
         private readonly IProductRepository _productRepository;
         private readonly IMapper _mapper;
 
-        public ProductServiceWithNoCaching(IGenericRepository<Product> repository, IUnitOfWork unitOfWork, IProductRepository productRepository, IMapper mapper) : base(repository, unitOfWork)
+        public ProductService(IGenericRepository<Product> repository, IUnitOfWork unitOfWork, IProductRepository productRepository, IMapper mapper) : base(repository, unitOfWork)
         {
             _productRepository = productRepository;
             _mapper = mapper;
