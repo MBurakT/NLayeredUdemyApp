6eb3d111aef0f28fa449ab2f408c0adb583a8639
fkymon <mbtbrk11@outlook.com>
2022-10-08 12:41:33 +0300

    Bölüm 9.59

--- NLayer.Api/Controllers/CategoriesController.cs
+++ NLayer.Api/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.Dtos;
 using NLayer.Core.Services;
 
 namespace NLayer.Api.Controllers
@@ -8,12 +9,23 @@ namespace NLayer.Api.Controllers
     public class CategoriesController : CustomBaseController
     {
         private readonly IMapper _mapper;
-        private ICategoryService _categoryService;
+        private readonly ICategoryService _categoryService;
 
         public CategoriesController(IMapper mapper, ICategoryService categoryService)
         {
             _mapper = mapper;
             _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var categories = await _categoryService.GetAllAsync();
+
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            return CreateActionResult(CustomResponseDto<List<CategoryDto>>.Success(200, categoriesDto));
+
         }
 
         [HttpGet("[action]/{id}")]
