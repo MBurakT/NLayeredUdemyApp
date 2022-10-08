bbf3ddb13e9f267db8a591287660dd83bbe5a8eb
fkymon <mbtbrk11@outlook.com>
2022-10-08 12:07:24 +0300

    Bölüm 9.58

--- NLayer.Web/Services/ProductApiService.cs
+++ NLayer.Web/Services/ProductApiService.cs
@@ -1,4 +1,6 @@
-﻿namespace NLayer.Web.Services
+﻿using NLayer.Core.Dtos;
+
+namespace NLayer.Web.Services
 {
     public class ProductApiService
     {
@@ -9,6 +11,42 @@
             _httpClient = httpClient;
         }
 
+        public async Task<List<ProductWithCategoryDto>> GetProductsWithCategoryAsync()
+        {
+            var response = await _httpClient.GetFromJsonAsync<CustomResponseDto<List<ProductWithCategoryDto>>>("Products/GetProductsWithCategory");
 
+            return response.Data;
+        }
+
+        public async Task<ProductDto> SaveAsync(ProductDto productDto)
+        {
+            var response = await _httpClient.PostAsJsonAsync("Products", productDto);
+
+            if (!response.IsSuccessStatusCode) return null;
+
+            var responseBody = await response.Content.ReadFromJsonAsync<CustomResponseDto<ProductDto>>();
+
+            return responseBody.Data;
+        }
+
+        public async Task<ProductDto> GetByIdAsync(int id)
+        {
+            var response = await _httpClient.GetFromJsonAsync<CustomResponseDto<ProductDto>>($"Products/{id}");
+            return response.Data;
+        }
+
+        public async Task<bool> UpdateAsync(ProductDto productDto)
+        {
+            var response = await _httpClient.PutAsJsonAsync("Products", productDto);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> RemoveAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"Products/{id}");
+
+            return response.IsSuccessStatusCode;
+        }
     }
 }
